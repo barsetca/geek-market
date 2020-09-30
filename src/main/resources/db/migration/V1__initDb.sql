33a0dc05afe99635c558554c7063e7b2ad2e89ae
Viktor Cherniak <barsetca@gmail.com>
2020-09-30 10:32:24 +0300

    hw05 changed title of modal

--- src/main/resources/db/migration/V1__initDb.sql
+++ src/main/resources/db/migration/V1__initDb.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS products;
 
 CREATE TABLE IF NOT EXISTS products
 (
-    id    bigint AUTO_INCREMENT PRIMARY KEY,
+    id    bigserial PRIMARY KEY,
     title VARCHAR(255) UNIQUE NOT NULL,
     cost  INTEGER
 );
