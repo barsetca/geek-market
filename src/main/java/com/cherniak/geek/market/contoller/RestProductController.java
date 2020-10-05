8306c7ba219ed108960a5a2ab19a2f5a1b9e4032
Viktor Cherniak <barsetca@gmail.com>
2020-10-05 12:10:35 +0300

    hw07 + filter

--- src/main/java/com/cherniak/geek/market/contoller/RestProductController.java
+++ src/main/java/com/cherniak/geek/market/contoller/RestProductController.java
@@ -2,11 +2,15 @@ package com.cherniak.geek.market.contoller;
 
 import com.cherniak.geek.market.model.Product;
 import com.cherniak.geek.market.service.ProductService;
+import com.cherniak.geek.market.util.ProductFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -16,9 +20,29 @@ public class RestProductController {
     ProductService productService;
 
     @GetMapping
-    public List<Product> getAll() {
-        return productService.findAll(Specification.where(null), 0, 10).getContent();
+    public List<Product> getAll(@RequestParam Map<String,String> params)
+    {
+        System.out.println("Пришли сюда с парметрами " + params);
+        ProductFilter productFilter = new ProductFilter(params);
+        return productService.findAll(productFilter.getSpec(), 0, 10).getContent();
     }
+
+//    public String findProducts(Model model,
+//                               @RequestParam(defaultValue = "1", name = "page") Integer page,
+//                               @RequestParam(defaultValue = "5", required = false) Integer size,
+//                               @RequestParam Map<String, String> params) {
+//
+//        page = page < 1 ? 1 : page;
+//        if (size < 1) {
+//            size = 1;
+//            params.put("size", "1");
+//        }
+//        ProductFilter productFilter = new ProductFilter(params);
+//        Page<Product> products = productService.findAll(productFilter.getSpec(), page - 1, size);
+//        model.addAttribute("products", products);
+//        model.addAttribute("filterDefinition", productFilter.getFilterDefinition());
+//        return "products";
+//    }
 
     @GetMapping("/{id}")
     public Product getById(@PathVariable Long id) {
--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -2,12 +2,22 @@ angular.module('app').controller('storeController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/market';
 
     $scope.fillTable = function () {
-        console.log('fill');
         $http.get(contextPath + '/api/v1/products')
             .then(function (response) {
                 $scope.Products = response.data;
             });
     };
+
+
+    // $scope.applyFilter = function () {
+    //     $http({
+    //         url: contextPath + '/api/v1/books',
+    //         method: "GET",
+    //         params: {obj_title: $scope.obj.title, obj_price: $scope.obj.price}
+    //     }).then(function (response) {
+    //         ...
+    //     });
+    // }
 
     $scope.submitCreateNewProduct = function () {
         $http.post(contextPath + '/api/v1/products', $scope.newProduct)
@@ -18,5 +28,16 @@ angular.module('app').controller('storeController', function ($scope, $http) {
             });
     };
 
+    $scope.filter = function () {
+       $http({
+           url: contextPath + '/api/v1/products',
+           method: "GET",
+           params: {title: $scope.newFilter.title, min_cost: $scope.newFilter.min_cost, max_cost: $scope.newFilter.max_cost}
+       }).then(function (response){
+           $scope.Products = response.data;
+        });
+    };
+
     $scope.fillTable();
+
 });
