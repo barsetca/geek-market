24b5348bc5de0789b5d58e97249de7384f50fbde
Viktor Cherniak <barsetca@gmail.com>
2020-10-16 00:26:26 +0300

    OrderItemDto

--- src/main/java/com/cherniak/geek/market/contoller/RestCartController.java
+++ src/main/java/com/cherniak/geek/market/contoller/RestCartController.java
@@ -1,11 +1,12 @@
 package com.cherniak.geek.market.contoller;
 
+import com.cherniak.geek.market.dto.OrderItemDto;
+import com.cherniak.geek.market.dto.OrderItemMapper;
 import com.cherniak.geek.market.exception.ResourceNotFoundException;
 import com.cherniak.geek.market.model.Product;
 import com.cherniak.geek.market.service.ProductService;
 import com.cherniak.geek.market.util.Cart;
-import com.cherniak.geek.market.util.CartTO;
-import com.cherniak.geek.market.util.OrderItemTO;
+import com.cherniak.geek.market.util.CartDto;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,34 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/restcart")
 @AllArgsConstructor
 public class RestCartController {
-
+//@Autowired
     private ProductService productService;
+//@Autowired
     private Cart cart;
 
     @GetMapping
-    public CartTO getCurrentCart() {
+    public CartDto getCurrentCart() {
 
-        List<OrderItemTO> orderItemTOS = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            OrderItemTO itemTO = new OrderItemTO();
-            itemTO.setCost(i);
-            itemTO.setQuantity(i);
-            itemTO.setTitle("title" + i);
-            itemTO.setTotalCost(4);
-            orderItemTOS.add(itemTO);
-        }
-        CartTO cartTO = new CartTO();
-        cartTO.setItems(orderItemTOS);
-        cartTO.setPrice(16);
-        cartTO.setTotalQuantity(16);
-        return cartTO;
+        List<OrderItemDto> orderItemDtos = OrderItemMapper.MAPPER.fromOrderItemList(cart.getItems());
+        CartDto cartDto = new CartDto();
+        cartDto.setPrice(cart.getPrice());
+        cartDto.setTotalQuantity(cart.getTotalQuantity());
+        cartDto.setItems(orderItemDtos);
+        return cartDto;
     }
 
     @GetMapping("/add/{product_id}")
