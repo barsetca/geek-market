6a0dce74f1ddce02e94b992b61da4843249a6c23
Viktor Cherniak <barsetca@gmail.com>
2020-11-06 16:38:43 +0300

    hw03 profile.html used value and refactoring profile.js

--- src/main/resources/static/profile/profile.js
+++ src/main/resources/static/profile/profile.js
@@ -10,37 +10,21 @@ angular.module('app').controller('profileController', function ($scope, $http) {
     .then(function (response) {
       console.log(response.data);
       $scope.profile = response.data;
+      $scope.oldUserName = $scope.profile.username;
+      $scope.oldBirtday = $scope.profile.birthday;
     });
   };
 
   $scope.submitChangeProfile = function () {
-    console.log($scope.newProfile.password);
-    $scope.newProfile.userId = $scope.profile.userId;
-    $scope.newProfile.id = $scope.profile.id;
-
-    $scope.newProfile.username = $scope.newProfile.username == null
-        ? $scope.profile.username : $scope.newProfile.username;
-    if ($scope.newProfile.username != $scope.profile.username){
-      alert('Вы изменили Nickname! После завршения изменения профиля, пожалуйста, '
-          + 'введите повтороно новое имя и действующий пароль на форме входа.'
+    console.log($scope.profile.birthday)
+    if ($scope.oldUserName != $scope.profile.username){
+      alert('Вы изменили Nickname! После сообщения "Профиль изменен", пожалуйста, '
+          + 'введите повторно новое имя и действующий пароль на форме входа.'
           + ' Форма откроется автоматически после обновления профиля. Спасибо.');
     }
-    $scope.newProfile.email = $scope.newProfile.email == null
-        ? $scope.profile.email : $scope.newProfile.email;
-    $scope.newProfile.phone = $scope.newProfile.phone == null
-        ? $scope.profile.phone : $scope.newProfile.phone;
-    $scope.newProfile.firstname = $scope.newProfile.firstname == null
-        ? $scope.profile.firstname : $scope.newProfile.firstname;
-    $scope.newProfile.surname = $scope.newProfile.surname == null
-        ? $scope.profile.surname : $scope.newProfile.surname;
-    $scope.newProfile.birthday = $scope.newProfile.birthday == null
-        ? $scope.profile.birthday : $scope.newProfile.birthday;
-    $scope.newProfile.sex = $scope.newProfile.sex == null ? $scope.profile.sex
-        : $scope.newProfile.sex;
-    $scope.newProfile.city = $scope.newProfile.city == null
-        ? $scope.profile.city : $scope.newProfile.city;
+    $scope.profile.birthday = $scope.profile.birthday == null ? $scope.oldBirtday : $scope.profile.birthday;
 
-    $http.put(contextPath + '/api/v1/profile', $scope.newProfile)
+    $http.put(contextPath + '/api/v1/profile', $scope.profile)
     .then(function (response) {
       $(".modal").modal("hide");
       $scope.getProfile();
@@ -49,8 +33,9 @@ angular.module('app').controller('profileController', function ($scope, $http) {
   };
 
   $scope.tryToAuth = function () {
-    console.log($scope.profile.username);
-    $scope.user.username = $scope.profile.username;
+    console.log($scope.oldUserName);
+    // $scope.user.username = $scope.profile.username;
+    $scope.user.username = $scope.oldUserName;
     console.log($scope.user.username);
     $http.post(contextPath + '/api/v1/profile', $scope.user)
     .then(function successCallback(response) {
@@ -59,7 +44,7 @@ angular.module('app').controller('profileController', function ($scope, $http) {
       $scope.submitChangeProfile();
 
     }, function errorCallback(response) {
-      window.alert('Введен Неверный пароль. Нажмите Ок, затем F5 и снова войдите в профиль');
+      window.alert('Введен неверный пароль!');
       $scope.getProfile();
     });
   };
